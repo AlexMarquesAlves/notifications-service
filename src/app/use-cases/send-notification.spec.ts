d3d97d30c8c2fdc31d7fdfcc6ca2bd0bbd35b1eb
¡Duque <atanaelleonardo@gmail.com>
2022-12-14 00:27:40 -0300

    𝑫𝒆𝒄𝒐𝒖𝒑𝒍𝒆𝒅 𝒊𝒏-𝒎𝒆𝒎𝒐𝒓𝒚 𝒅𝒂𝒕𝒂𝒃𝒂𝒔𝒆

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,24 +1,18 @@
-import { Notification } from '../entities/notification';
+import { InMemoryNotificationRepository } from '../../../test/repositories/in-memory-notifications-repositories';
 import { SendNotification } from './send-notification';
-
-const notifications: Notification[] = [];
-
-const notificationRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
 
 describe(`Send Notification`, () => {
   it('should be able to create a notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
     const sendNotification = new SendNotification(notificationRepository);
 
-    await sendNotification.execute({
+    const { notification } = await sendNotification.execute({
       content: `This is a notification`,
       category: `social`,
       recipientId: `example-recipient-id`,
     });
 
-    expect(notifications).toHaveLength(1);
+    expect(notificationRepository.notifications).toHaveLength(1);
+    expect(notificationRepository.notifications[0]).toEqual(notification);
   });
 });
