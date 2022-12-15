4d54a48cad80f54a75629009513ee9f018db4d00
¡Duque <atanaelleonardo@gmail.com>
2022-12-15 00:14:13 -0300

    𝑪𝒐𝒏𝒇𝒊𝒈𝒖𝒓𝒆𝒅 𝑻𝑺 𝒑𝒂𝒕𝒉𝒔

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,5 +1,5 @@
+import { Replace } from '@helpers/replace';
 import { randomUUID } from 'node:crypto';
-import { Replace } from 'src/helpers/replace';
 import { Content } from './content';
 
 export interface NotificationProps {
