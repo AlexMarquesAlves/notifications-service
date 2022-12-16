88b70df2c90ffcd2a8848c5ff7d16f82d725be91
¡Duque <atanaelleonardo@gmail.com>
2022-12-16 01:53:47 -0300

    𝒊𝒎𝒑𝒍𝒆𝒎𝒆𝒏𝒕𝒔 𝒏𝒆𝒘𝒔 𝒎𝒆𝒕𝒉𝒐𝒅𝒔

--- test/repositories/in-memory-notifications-repositories.ts
+++ test/repositories/in-memory-notifications-repositories.ts
@@ -4,7 +4,15 @@ import { NotificationsRepository } from '@app/repositories/notification-reposito
 export class InMemoryNotificationRepository implements NotificationsRepository {
   public notifications: Notification[] = [];
 
+  async findById(notificationId: string): Promise<Notification> {
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
